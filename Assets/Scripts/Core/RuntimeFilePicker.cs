912dadad86ca3af1397c676df824947509339af2
TheSei <theseipartofme@gmail.com>
2025-08-17 19:11:21 +0200

    Add plagin
    adding windows.forms.dll.

--- Assets/Scripts/Core/RuntimeFilePicker.cs
+++ Assets/Scripts/Core/RuntimeFilePicker.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
 
+using SForms = System.Windows.Forms;
 public class RuntimeFilePicker : IFilePicker
 {
     public string OpenImageFile()
     {
-#if SFB_ENABLED
-        var exts = new[] {new SFB.ExtentionFilter("Image", "png", "jpg", "jpeg")};
-        var res = SFB.StandaloneFileBrowser.OpenFilePanel("Select Logo", "", exts, false);
-        return (res != null && res.Length > 0) ? res[0] : null;
-#else
-        Debug.LogWarning("RuntimeFilePicker: SFB not installed.");
-        return null;
+#if UNITY_STANDALONE_WIN && !UNITY_EDITOR
+        try
+        {
+            using (var ofd = new SForms.OpenFileDialog())
+            {
+                ofd.Title = "Select Logo";
+                ofd.Filter = "Image files (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg";
+                ofd.Multiselect = false;
+
+                if (ofd.ShowDialog() == SForms.DialogResult.OK)
+                {
+                    return ofd.FileName;
+                }
+            }
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("RuntimeFilePicker: " + ex.Message);
+        }
 #endif
+        Debug.LogWarning("RuntimeFilePicker: File dialog not available.");
+        return null;
     }
 }
